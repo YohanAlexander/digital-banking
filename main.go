672f6b5509a57dfea07d67b2db9cecc99b027913
Yohan Alexander <yohanfranca@gmail.com>
2020-11-28 09:43:31 -0300

    ✨ iniciando um servidor basico

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
